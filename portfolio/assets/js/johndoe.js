50aa6aad4aaf2f4f541db66706a0cfaa2636f466
Mario-Marchio <mariomarchio1610@gmail.com>
2023-12-02 09:53:15 +0100

    last minor fix

--- portfolio/assets/js/johndoe.js
+++ portfolio/assets/js/johndoe.js
@@ -100,3 +100,61 @@ $(document).ready(function () {
     });
   });
 });
+
+$(document).ready(function () {
+  var showMoreButton = $("#show-more-button");
+  var showLessButton = $("#show-less-button");
+
+  // Funzione per controllare se la larghezza della finestra è inferiore a 768px (modalità telefono)
+  function isMobile() {
+    return $(window).width() < 768;
+  }
+
+  // Imposta il margine del contenitore al caricamento della pagina in modalità telefono
+  if (isMobile()) {
+    showMoreButton.parent().css("margin-top", "-200px");
+  }
+
+  // Gestisci il click sul pulsante "Mostra altri"
+  showMoreButton.on("click", function (e) {
+    e.preventDefault();
+
+    // Cambia il margine del contenitore solo in modalità telefono
+    if (isMobile()) {
+      showMoreButton
+        .parent()
+        .animate({ marginTop: "0" }, 500, "swing", function () {
+          // Al termine dell'animazione (opzionale), puoi eseguire ulteriori azioni qui
+        });
+    }
+
+    // Puoi nascondere il pulsante "Mostra altri" se necessario
+    showMoreButton.hide();
+    // Mostra il pulsante "Mostra meno"
+    showLessButton.show();
+  });
+
+  // Gestisci il click sul pulsante "Mostra meno"
+  showLessButton.on("click", function (e) {
+    e.preventDefault();
+
+    // Nascondi il pulsante "Mostra meno"
+    showLessButton.hide();
+    // Mostra il pulsante "Mostra altri"
+    showMoreButton.show();
+
+    // Cambia il margine del contenitore solo in modalità telefono
+    if (isMobile()) {
+      showMoreButton
+        .parent()
+        .animate({ marginTop: "-200px" }, 500, "swing", function () {
+          // Al termine dell'animazione (opzionale), puoi eseguire ulteriori azioni qui
+        });
+    }
+  });
+
+  // Aggiungi un evento di ridimensionamento per gestire la larghezza della finestra
+  $(window).resize(function () {
+    // Puoi aggiornare il comportamento in base alla larghezza della finestra qui, se necessario
+  });
+});
