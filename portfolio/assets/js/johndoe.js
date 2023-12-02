3a24993a162684861d9a65bf7c8fe5b4def45a98
Mario-Marchio <mariomarchio1610@gmail.com>
2023-12-02 09:15:53 +0100

    finished

--- portfolio/assets/js/johndoe.js
+++ portfolio/assets/js/johndoe.js
@@ -31,11 +31,97 @@ $(document).ready(function () {
       );
     }
   });
-});
-$(".slick-carousel").slick({
-  infinite: true,
-  slidesToShow: 3, // Shows a three slides at a time
-  slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
-  arrows: true, // Adds arrows to sides of slider
-  dots: true, // Adds the dots on the bottom
+
+  var servicesContainer = $(".row");
+  var services = servicesContainer.find(".box");
+  var showMoreButton = $("#show-more-button");
+  var showLessButton = $("#show-less-button");
+
+  // Initial display counts for different devices
+  var desktopDisplayCount = 6;
+  var tabletDisplayCount = 4;
+  var mobileDisplayCount = 2;
+
+  // Set initial display based on device type
+  setInitialDisplay();
+
+  // Function to set initial display based on device type
+  function setInitialDisplay() {
+    var windowWidth = $(window).width();
+    var displayCount;
+
+    if (windowWidth >= 992) {
+      // Desktop
+      displayCount = desktopDisplayCount;
+    } else if (windowWidth >= 768) {
+      // Tablet
+      displayCount = tabletDisplayCount;
+    } else {
+      // Mobile
+      displayCount = mobileDisplayCount;
+    }
+
+    services.hide().slice(0, displayCount).show();
+    showMoreButton.show();
+    showLessButton.hide();
+  }
+
+  // Update display on window resize
+  $(window).resize(function () {
+    setInitialDisplay();
+  });
+
+  // Handle click on "Mostra altri" button
+  showMoreButton.on("click", function (e) {
+    e.preventDefault();
+
+    // Memorizza la posizione prima dell'operazione
+    var currentPosition = $(window).scrollTop();
+
+    services.slideDown();
+    showMoreButton.hide();
+    showLessButton.show();
+
+    // Add style to center new services
+    servicesContainer.find(".box:hidden").css({
+      // Add styles necessary to center the new services
+    });
+
+    // Scroll down after showing more services
+    $("html, body").animate(
+      {
+        scrollTop: currentPosition + servicesContainer.height(),
+      },
+      {
+        duration: 500,
+        easing: "swing",
+        complete: function () {
+          // Completa la tua logica qui se necessario
+        },
+      }
+    );
+
+    // Prevent the default behavior of the anchor link
+    e.preventDefault();
+  });
+
+  // Handle click on "Mostra meno" button
+  showLessButton.on("click", function (e) {
+    e.preventDefault();
+
+    // Memorizza la posizione prima dell'operazione
+    var currentPosition = $(window).scrollTop();
+
+    services.slideUp(function () {
+      setInitialDisplay();
+
+      // Scroll to the previous position
+      $("html, body").animate(
+        {
+          scrollTop: currentPosition,
+        },
+        500
+      );
+    });
+  });
 });
